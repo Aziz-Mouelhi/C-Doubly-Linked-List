30412a2cf62e140d5683ad75e34952ae921ce2fb
Mouelhi-Aziz <aziz1working@gmail.com>
2023-08-25 13:45:15 +0200

    last

--- Doubly/data.h
+++ Doubly/data.h
@@ -8,10 +8,14 @@ typedef struct {
 }data;
 
 
-data *Create_Node(data *head , int x); // create the first node //
+data *Create_Node(data *head ,int x); // create the first node //
+
 void AddEnd(data *head); // add elements from the beginning//
+
 void PrintData(data *head);
+
 data *AddFirst(data *head);
+
 int Counter(data *head);
 
 #endif // DATA_H_INCLUDED
